d3f3d3a561c0e54a40732fa9fae65367f60a6e16
Ravish <ravish1729@gmail.com>
2021-06-06 06:22:13 +0530

    list view

--- src/icdrive_assets/src/App.jsx
+++ src/icdrive_assets/src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Dashboard from './components/Dashboard';
+import 'antd/dist/antd.css';
 
 const App = () => {
   return (
--- src/icdrive_assets/src/state/reducers/index.jsx
+++ src/icdrive_assets/src/state/reducers/index.jsx
@@ -1,5 +1,6 @@
 import {combineReducers} from 'redux'
+import FileHandler from './FileHandler'
 
 export default combineReducers({
-    
+  FileHandler
 })
