9be3ce2217cc38c288c43f12f857598c8ec957be
Ravish <ravish1729@gmail.com>
2021-06-07 14:12:02 +0530

    file download

--- src/icdrive_assets/src/components/CenterPortion/CenterPortion.jsx
+++ src/icdrive_assets/src/components/CenterPortion/CenterPortion.jsx
@@ -6,32 +6,13 @@ import ListView from './ListView';
 import GridView from './GridView';
 
 // 3rd party imports
-//import * as streamSaver from 'streamsaver';
-//import { WritableStream } from 'web-streams-polyfill/ponyfill'
 import {MenuOutlined, AppstoreOutlined} from "@ant-design/icons";
 import {useSelector} from 'react-redux'
 
 const CenterPortion = () =>{
 
   const [selectedView, setSelectedView] = React.useState("listView");
-  const upload = useSelector(state=>state.FileHandler.upload)
-  
-  /*const download = async (fileId, chunk_count, fileName) => {
-    streamSaver.WritableStream = WritableStream
-    const fileStream = streamSaver.createWriteStream(fileName);
-    const writer = fileStream.getWriter();
-    for(let j=0; j<chunk_count; j++){
-      const bytes = await icdrive.getFileChunk(fileId, j+1);
-      //const bytesAsBuffer = Buffer.from(new Uint8Array(bytes[0]));
-      const bytesAsBuffer = new Uint8Array(bytes[0]);
-      writer.write(bytesAsBuffer);
-    }
-    writer.close();
-  };
-
-  const handleDownload = async (fileId, chunk_count, fileName) =>{
-    let k = await download(fileId, chunk_count, fileName)
-  }*/
+  const upload = useSelector(state=>state.FileHandler.upload);
 
   return(
     <Style>
