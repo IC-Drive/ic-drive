7c8d0e8eee6b477fcf72e7b8c709e2eda021f42c
Ravish <ravish1729@gmail.com>
2021-06-07 14:12:02 +0530

    file download

--- src/icdrive_assets/src/components/CenterPortion/ListView.jsx
+++ src/icdrive_assets/src/components/CenterPortion/ListView.jsx
@@ -4,8 +4,10 @@ import styled from 'styled-components';
 // custom imports
 
 // 3rd party imports
-//import * as streamSaver from 'streamsaver';
-//import { WritableStream } from 'web-streams-polyfill/ponyfill'
+import * as streamSaver from 'streamsaver';
+import { WritableStream } from 'web-streams-polyfill/ponyfill'
+import { Actor, HttpAgent } from '@dfinity/agent';
+import { idlFactory as icdrive_idl, canisterId as icdrive_id } from 'dfx-generated/icdrive';
 import {useSelector} from 'react-redux';
 import {DownloadOutlined, DeleteOutlined, EditOutlined} from "@ant-design/icons";
 import {Table, Popconfirm} from 'antd';
@@ -13,6 +15,33 @@ import {Table, Popconfirm} from 'antd';
 const ListView = () =>{
 
   const files = useSelector(state=>state.FileHandler.files)
+
+  const agent = new HttpAgent();
+  const icdrive = Actor.createActor(icdrive_idl, { agent, canisterId: icdrive_id });
+
+  const download = async (fileId, chunk_count, fileName) => {
+    streamSaver.WritableStream = WritableStream
+    streamSaver.mitm = 'http://localhost:8000/mitm.html'
+    const fileStream = streamSaver.createWriteStream(fileName);
+    const writer = fileStream.getWriter();
+    for(let j=0; j<chunk_count; j++){
+      const bytes = await icdrive.getFileChunk(fileId, j+1);
+      //const bytesAsBuffer = Buffer.from(new Uint8Array(bytes[0]));
+      const bytesAsBuffer = new Uint8Array(bytes[0]);
+      writer.write(bytesAsBuffer);
+    }
+    writer.close();
+  };
+
+  const handleDownload = async (fileId, chunk_count, fileName) =>{
+    let k = await download(fileId, chunk_count, fileName)
+  }
+
+  const downloadFile = async () =>{
+    console.log("here")
+    const k = await handleDownload(files[0]["fileId"], files[0]["chunkCount"], files[0]["name"])
+    console.log("here over")
+  }
 
   const columns = [
     {
@@ -38,7 +67,7 @@ const ListView = () =>{
         return (
         <span>
           <a>
-            <DownloadOutlined />&nbsp;&nbsp;
+            <DownloadOutlined onClick={downloadFile} />&nbsp;&nbsp;
           </a>
           <a>
             <EditOutlined />&nbsp;&nbsp;
