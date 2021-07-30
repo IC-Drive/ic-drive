978883e2531d6c6d4f79c6691f233d8607d68a3b
Ravish <ravish1729@gmail.com>
2021-07-30 08:40:06 +0530

    fix public view design issue

--- src/icdrive_assets/src/components/PublicUrl.jsx
+++ src/icdrive_assets/src/components/PublicUrl.jsx
@@ -13,6 +13,7 @@ import { httpAgent, httpAgentIdentity } from '../httpAgent';
 const PublicUrl = () => {
   const [notFound, setNotFound] = React.useState(false);
   const [data, setData] = React.useState('');
+  const [type, setType] = React.useState('');
 
   const isPdf = (mimeType) =>{
     let flag = false
@@ -27,9 +28,9 @@ const PublicUrl = () => {
       const icdrive = await httpAgent();
       const temp = window.location.href.split('/');
       const hash = temp[temp.length - 1];
-      // console.log(hash);
+      
       const file = await icdrive.getPublicFileLocation(hash);
-      // console.log(file);
+
       if (file.length === 1) {
         const metaData = file[0].split('$');
 
@@ -59,7 +60,6 @@ const PublicUrl = () => {
           const chunkBuffers = [];
           for (let j = 0; j < chunkCount; j += 1) {
             const bytes = await userAgentShare.getPublicFileChunk(fileId, j + 1);
-            // console.log(bytes);
             const bytesAsBuffer = new Uint8Array(bytes[0]);
             chunkBuffers.push(bytesAsBuffer);
           }
@@ -69,6 +69,7 @@ const PublicUrl = () => {
           });
 
           const fileURL = URL.createObjectURL(fileBlob);
+          setType(mimeType);
           setData(fileURL);
           //window.open(fileURL, '_self');
           
@@ -92,7 +93,14 @@ const PublicUrl = () => {
             />
           )
           :
-          <object data={data} id = "object-tag" />
+            isPdf(type)?
+            <div className="show-pdf">
+              <embed name="316B4D6EC81C66DEFBBB62C94CA2068C" style={{position:"absolute", left: "0", top: "0"}} width="100%" height="100%" src={data} type="application/pdf" internalid="316B4D6EC81C66DEFBBB62C94CA2068C" />
+            </div>
+            :
+            <div className="show-image">
+              <img id="the-image" src={data}/>
+            </div>
       }
     </Style>
   );
@@ -102,12 +110,26 @@ export default PublicUrl;
 
 const Style = styled.div`
   font-style: sans-serif;
-  box-sizing: border-box;
-  width: 100vw;
   height: 100vh;
+  width: 100vw;
 
-  #object-tag{
-    height: 100vh;
-    width: 100vw;
+  .show-pdf{
+    height: 100%;
+    width: 100%;
+    overflow: hidden;
+    margin: 0px;
+    background-color: rgb(82, 86, 89);
+  }
+  .show-image{
+    display: flex;
+    margin: 0px;
+    background: #0e0e0e;
+    height: 100%;
+  }
+  #the-image{
+    -webkit-user-select: none;
+    margin: auto;
+    background-color: hsl(0, 0%, 90%);
+    transition: background-color 300ms;
   }
 `;
